ab21bac18be76c50395bfcddf5eb6e33caf2f74a
Dhafer84 <boutheljad84@gmail.com>
2022-05-13 23:09:20 +0100

    JS

--- js/scripts.js
+++ js/scripts.js
@@ -65,6 +65,48 @@ function verif_signup(event){
     }
 
  }
- function verif_login(){
+ function verif_login(event){
+    event.preventDefault();
+    var lmail = document.forms["forml"]["lmail"];
+    var pattern =/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    var mdp = document.forms["forml"]["psw"];
+    var pattern2= /^(?=\S*[a-z])(?=\S*[A-Z])(?=\S*\d)(?=\S*[^\w\s])\S{8,}$/;
+
+    if(!lmail.value) {
+        document.getElementById('erroremail').innerHTML= "Veuillez saisir votre mail";
+        lmail.focus();
+        return false;
+    } else {
+        document.getElementById('erroremail').innerHTML="";
+    }
+
+
+    if (!mdp.value) {
+        document.getElementById('errormdp').innerHTML= "Veuillez saisir votre mot de passe";
+        mdp.focus();
+        return false;
+    } else {
+        document.getElementById('errormdp').innerHTML="";
+    }
+
+    if(!mdp.value.match(pattern2)) {
+        document.getElementById('errormdp').innerHTML= "Le mot de passe doit contenir au moins 8 caractères, dont au moins : Une lettre majuscule, Une lettre minuscule et un nombre et un caractére speciale";
+        mdp.focus();
+        return false;
+    } else{
+            document.getElementById('errormdp').innerHTML="";
+    }
+
+    if (!lmail.value.match(pattern)) {
+        document.getElementById("errorcemail").innerHTML= "Veuillez vérifier votre mail";
+        lmail.focus();
+        return false;
+    } else {
+        document.getElementById("errorcemail").innerHTML="";
+    }
+
+    
+
+    
      
  }
